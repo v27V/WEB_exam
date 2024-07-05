d4e2d066df414193b0d5429a2918255ab36cf3c7
v27V <133388843+v27V@users.noreply.github.com>
2024-07-06 01:03:25 +1000

    Add files via upload

---
+++ db.php
@@ -0,0 +1,34 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "kali";
+    $dbName = "db_first";
+    $link = mysqli_connect($servername, $username, $password);
+
+    if (!$link) {
+        die("Ошибка подключения: " . mysqli_connection_error());
+    }
+
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbName";
+
+    if (!mysqli_query($link, $sql)) {
+        echo "Не удалось создать БД";
+    }
+
+    mysqli_close($link);
+
+    $link = mysqli_connect($servername, $username, $password, $dbName);
+
+     $sql = "CREATE TABLE IF NOT EXISTS users(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(15) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        pass VARCHAR(20) NOT NULL
+    )";
+
+    if(!mysqli_query($link, $sql)) {
+        echo "Не удалось создать таблицу Users";
+    }
+
+    mysqli_close($link);
+?>
---
+++ registration.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Толмачев В.В. </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container">
+        <div class="row">
+                <div class="col-12 ">
+                    <h1> Регистрация </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 ">
+                    <form method='POST' action='/registration.php'>
+                        <div class="row form__reg"><input class="form" type="email" name="email" placeholder="Email"></div>
+                        <div class="row form__reg"><input class="form" type="text" name="login" placeholder="Login"></div>
+                        <div class="row form__reg"><input class="form" type="password" name="password" placeholder="Password"></div>
+                        <button type="submit" class="btn_red btn__reg" name="submit">Продолжить</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<?php
+    require_once('db.php');
+
+    if (isset($_COOKIE['User'])) {
+        header("Location: login.php");
+    }
+
+    $link = mysqli_connect('127.0.0.1', 'root', 'kali', 'db_first');
+
+
+    if (isset($_POST['submit'])) {
+        $email = $_POST['email'];
+        $username = $_POST['login'];
+        $password = $_POST['password'];
+
+        if (!$email || !$username || !$password) die ('Пожалуйста введите все значения!');
+
+        $sql = "INSERT INTO users (email, username, pass) VALUES ('$email', '$username', '$password')";
+
+        if(!mysqli_query($link, $sql)) {
+            echo "Ошибка, не удалось добавить пользователя";
+        }
+    }
+?>
